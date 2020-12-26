ed78287761d5aca49aa452484758698b6504edfa
Winston Van <wvan@laurentian.ca>
2020-12-26 14:48:27 -0500

    education + experience
    combined education section with experience

--- personal-website-v1/src/components/Experience.js
+++ personal-website-v1/src/components/Experience.js
@@ -5,6 +5,26 @@ function Experience() {
     return (
         <div className="experience">
             <div className="experience__header">
+                    EDUCATION
+            </div>
+
+            <div className="experiences">                
+                <div className="experience__item">
+                    <div className="experience__company__year">
+                        <div className="experience__company">
+                            Laurentian University
+                        </div>
+                        <div className="experience__year">
+                            September 2015 - May 2020
+                        </div>  
+                    </div>
+                    <div className="experience__position">
+                        Computer Science: Specialization in Web Data Management (B.CoSc)
+                    </div>
+                </div>  
+            </div>
+
+            <div className="experience__header">
                     EXPERIENCE
             </div>
 
