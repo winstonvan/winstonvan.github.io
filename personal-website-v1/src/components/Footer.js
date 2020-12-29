beaa5d88c4f581a26de22954ce0dc8e71448d7f2
Winston Van <48227172+winstonvan@users.noreply.github.com>
2020-12-29 15:40:32 -0500

    favicon

---
+++ personal-website-v1/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../styles/Footer.css"
+
+function Footer() {
+    return (
+        <section className="footer">
+            <div className="copyright">
+
+            </div>
+            <div className="links">
+                <span className="email">
+                    <a>Email</a>
+                </span>
+                <span className="linkedin">
+                    <a>Linkedin</a>
+                </span>
+                <span className="github">
+                    <a>Github</a>
+                </span>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
