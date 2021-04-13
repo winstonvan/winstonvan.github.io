b1477fbc15c93f1ec7bbe31364d53e8bec328c6f
Winston Van <wvan@laurentian.ca>
2021-04-12 21:31:33 -0400

    fixed css

--- personal-website-v1/src/components/Projects.js
+++ personal-website-v1/src/components/Projects.js
@@ -31,7 +31,13 @@ function Projects() {
             </div>
             <div className="project__description">
               A responsive and mobile-friendly portfolio website created using
-              React JS.
+              React.js.{" "}
+              <a
+                className="source"
+                href="https://github.com/winstonvan/winstonvan.github.io"
+              >
+                View source
+              </a>
             </div>
             <div className="project__languages">React</div>
             <div className="project__languages">CSS</div>
@@ -44,7 +50,7 @@ function Projects() {
             <div className="project__chat">
               <figure className="project__images">
                 <div className="laptop">
-                  <img src={"chat-app/mockup.png"} alt="" />
+                  <img src={"chat-app/mockup1.png"} alt="" />
                 </div>
               </figure>
             </div>
@@ -54,7 +60,7 @@ function Projects() {
                 target="_blank"
                 rel="noreferrer"
               >
-                A Simple Chat App
+                A Chat App
                 <span className="arrow">
                   <i class="fas fa-chevron-right"></i>
                 </span>
@@ -62,7 +68,13 @@ function Projects() {
             </div>
             <div className="project__description">
               A responsive and mobile-friendly chat application built using
-              React.js and Firebase
+              React.js and Firebase.{" "}
+              <a
+                className="source"
+                href="https://github.com/winstonvan/messenger-clone"
+              >
+                View source
+              </a>
             </div>
             <div className="project__languages">React</div>
             <div className="project__languages">CSS</div>
@@ -103,7 +115,10 @@ function Projects() {
               the first standardized psychometric tool to assess Internet Gaming
               Disorder (Pontes et al., 2014). This tool was inspired by the
               increasing gaming addictions and mental health concerns among
-              youths.
+              youths.{"  "}
+              <a className="source" href="https://github.com/winstonvan/Inform">
+                View source
+              </a>
             </div>
             <div className="project__languages">Android</div>
             <div className="project__languages">Java</div>
@@ -138,6 +153,13 @@ function Projects() {
                 <li className="item">Answer to other user's questions</li>
                 <li className="item">Chat (1:1)</li>
               </ul>
+              {"  "}
+              <a
+                className="source"
+                href="https://github.com/winstonvan/Studium"
+              >
+                View source
+              </a>
             </div>
             <div className="project__languages">Java</div>
             <div className="project__languages">Android</div>
@@ -210,6 +232,13 @@ function Projects() {
               This expert system was developed for a fourth year expert systems
               class. <br />A forward-chaining inference engine was used to infer
               data from a knowledge base.
+              {"  "}
+              <a
+                className="source"
+                href="https://github.com/winstonvan/ExpertProject1"
+              >
+                View source
+              </a>
             </div>
             <div className="project__languages">C#</div>
           </div>
